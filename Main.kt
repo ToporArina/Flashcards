c17217b20d3b348f0dad58f1f476a2f5328cc8a7
l000ps <toporoleksandr@gmail.com>
2023-05-12 11:18:07 +0300

    Stage 3/7: Make it your own

--- Main.kt
+++ Main.kt
@@ -1,12 +1,23 @@
 package flashcards
 
-val a = HashMap<String, String>()
+private val a:MutableMap<String, String> = mutableMapOf()
 
 fun main() {
-    a.put(readln(), readln())
-    if (a[a.keys.first()] == readln()) {
-        println("Your answer is right!")
-    } else {
-        println("Your answer is wrong...")
+    println("Input the number of cards:")
+    for (i in 0 until readln().toInt()) {
+        println("Card #${i + 1}:")
+        val k = readln()
+        println("The definition for card #${i + 1}:")
+        val v = readln()
+        a.put(k, v)
+    }
+
+    for (i in 0 until a.size) {
+        println("Print the definition of \"${a.keys.elementAt(i)}\"")
+        if (a[a.keys.elementAt(i)] == readln()) {
+            println("Correct!")
+        } else {
+            println("Wrong. The right answer is \"${a[a.keys.elementAt(i)]}\"")
+        }
     }
 }
