0e7ac8aaa7bfdb5884433f89209c33d904441c39
l000ps <toporoleksandr@gmail.com>
2023-05-25 20:19:03 +0300

    Stage 6/7: Statistics

--- Main.kt
+++ Main.kt
@@ -1,16 +1,21 @@
 package flashcards
 
 import java.io.File
+import kotlin.random.Random
 import kotlin.system.exitProcess
 
-private val a: MutableMap<String, String> = mutableMapOf()
+
+data class Card(val term: String, var def: String, var wrong: Int = 0)
+
+private var a: MutableList<Card> = mutableListOf()
 var card = ""
 var definition = ""
 var file = ""
+var logText = ""
 
 fun main() {
     while (true) {
-        println("Input the action (add, remove, import, export, ask, exit):")
+        println("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):")
         when (readln()) {
             "add" -> add()
             "remove" -> remove()
@@ -21,51 +26,118 @@ fun main() {
                 println("Bye bye!")
                 exitProcess(0)
             }
+            "log" -> log()
+            "hardest card" -> hardestCard()
+            "reset stats" -> reset()
         }
         println()
     }
 }
 
+fun reset() {
+    a.forEach { it.wrong = 0 }
+    println("Card statistics have been reset.")
+}
+
+fun hardestCard() {
+    a.sortWith(compareByDescending { it.wrong })
+    if (a.size == 0) {
+        println("There are no cards with errors.")
+        return
+    }
+    val wrongCards = mutableListOf<Card>()
+    if (a[0].wrong > 0) {
+        wrongCards.add(a[0])
+        if (a.size != 1) {
+            for (i in a.indices - 1) {
+                if (a[i].wrong == a[i + 1].wrong) {
+                    wrongCards.add(a[i + 1])
+                }
+            }
+        }
+        println(
+            "The hardest card${if (wrongCards.size == 1) " is" else "s are"} ${
+                wrongCards.map { it.term }.joinToString { "\"$it\"" }
+            }. You have ${wrongCards[0].wrong} errors answering ${if (wrongCards.size == 1) "it" else "them"}."
+        )
+    } else {
+        println("There are no cards with errors.")
+    }
+
+//    val hard = a[0].wrong
+//    if (a.size == 1) {
+//        if (hard == 0) {
+//            println("There are no cards with errors.")
+//        } else {
+//            println("The hardest card is \"${a[0].term}\". You have ${a[0].wrong} errors answering it.")
+//        }
+//        return
+//    }
+//    if (hard == 0) {
+//        println("There are no cards with errors.")
+//    } else if (a[0].wrong > a[1].wrong) {
+//        println("The hardest card is \"${a[0].term}\". You have ${a[0].wrong} errors answering it.")
+//    } else if (a[0].wrong == a[1].wrong) {
+//        println("The hardest cards are \"${a[0].term}\", \"${a[1].term}\". You have ${a[0].wrong} errors answering it.")
+//    }
+}
+
 fun add() {
     println("The card:")
     card = readln()
-    if (a.containsKey(card)) {
-        println("The card \"$card\" already exists.")
-        return
-    } else {
-        println("The definition of the card:")
-        definition = readln()
-        if (a.containsValue(definition)) {
-            println("The definition \"$definition\" already exists.")
+    for (i in 0 until a.size) {
+        if (a[i].term == card) {
+            println("The card \"$card\" already exists.")
             return
-        } else {
-            a.put(card, definition)
-            println("The pair (\"$card\":\"$definition\") has been added.")
         }
     }
+    println("The definition of the card:")
+    definition = readln()
+    for (i in 0 until a.size) {
+        if (a[i].def == definition) {
+            println("The definition \"$definition\" already exists.")
+            return
+        }
+    }
+    a.add(Card(card, definition, 0))
+    println("The pair (\"$card\":\"$definition\") has been added.")
+
+
 }
 
 fun remove() {
     println("Which card?")
     card = readln()
-    if (a.containsKey(card)) {
-        a.remove(card)
-        println("The card has been removed.")
-    } else {
-        println("Can't remove \"$card\": there is no such card.")
-        return
+    for (i in 0 until a.size) {
+        if (a[i].term == card) {
+            a.removeAt(i)
+            println("The card has been removed.")
+            return
+        }
     }
+    println("Can't remove \"$card\": there is no such card.")
 }
 
 fun import() {
+
     println("File name:")
     file = readln()
     if (File(file).exists()) {
         val list = File(file).readLines()
-        for (i in 0 until list.size step 2) {
-            a.put(list[i], list[i + 1])
+        var b = mutableListOf<Card>()
+        for (i in 0 until list.size step 3) {
+            b.add(Card(list[i], list[i + 1], list[i + 2].toInt()))
         }
-        println("${list.size / 2} cards have been loaded.")
+        for (card in b) {
+            val index = a.indexOfFirst { it.term == card.term }
+            if (index != -1) {
+                a[index].def = card.def
+            } else {
+                a.add(card)
+            }
+        }
+
+        println("${list.size / 3} cards have been loaded.")
     } else {
         println("File not found.")
         return
@@ -73,12 +145,13 @@ fun import() {
 }
 
 fun export() {
+
     println("File name:")
     file = readln()
     File(file).createNewFile()
     var str = ""
     for (i in 0 until a.size) {
-        str += "${a.keys.elementAt(i)}\n${a[a.keys.elementAt(i)]}\n"
+        str += "${a[i].term}\n${a[i].def}\n${a[i].wrong}\n"
     }
     File(file).writeText(str)
     println("${a.size} cards have been saved.")
@@ -87,20 +160,43 @@ fun export() {
 fun ask() {
     println("How many times to ask?")
     val times = readln().toInt()
-    for (i in 0 until times) {
-        println("Print the definition of \"${a.keys.elementAt(i % a.size)}\"")
-        var def = readln()
-        if (a[a.keys.elementAt(i % a.size)] == def) {
+    var nOfCard = 0
+    qu@ for (i in 0 until times) {
+        nOfCard = Random.nextInt(0, a.size)
+        println("Print the definition of \"${a[nOfCard].term}\":")
+        val def = readln()
+        if (a[nOfCard].def == def) {
             println("Correct!")
         } else {
-            print("Wrong. The right answer is \"${a[a.keys.elementAt(i % a.size)]}\"")
-            if (a.containsValue(def)) {
-                println(", but you definition is correct for \"${a.filter { def == it.value }.keys.first()}\".")
-            } else {
-                println(".")
+            a[nOfCard].wrong += 1
+            print("Wrong. The right answer is \"${a[nOfCard].def}\"")
+            for (i in 0 until a.size) {
+                if (a[i].def == def) {
+                    println(", but you definition is correct for \"${a[i].term}\".")
+                    continue@qu
+                }
             }
+            println(".")
         }
-
     }
+}
+
+fun log() {
+    println("File name:")
+    file = readln()
+    val log = File(file)
+    log.writeText(logText)
+    println("The log has been saved.")
+}
+
+fun println(str: String = "") {
+    kotlin.io.println(str)
+    logText += str + "\n"
+}
+
+fun readln(): String {
+    val str = kotlin.io.readln()
+    logText += ">$a\n"
+    return str
 }
 
